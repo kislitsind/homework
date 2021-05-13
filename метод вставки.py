33ab08a91dacf7c0396d91699e7021a0c9071445
kislitsind <72073197+kislitsind@users.noreply.github.com>
2021-05-14 02:29:38 +0500

    Add files via upload

---
+++ метод вставки.py
@@ -0,0 +1,19 @@
+import time
+
+start = time.time()
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i-1
+        while j >=0 and key < arr[j] :
+            arr[j+1] = arr[j]
+            j -= 1
+        arr[j+1] = key 
+arr = [12, 11, 13, 5, 6]
+insertion_sort(arr)
+print ("Sorted array is:")
+for i in range(len(arr)):
+    print (arr[i])
+
+    end = time.time()
+print(end - start)
